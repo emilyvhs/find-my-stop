22ce28ad02bfed33d2b4a8cf6ee689c414573100
Emily Sherwood <emily.sherwood@loucoll.ac.uk>
2026-09-07 14:17:33 +0100

    Started Heading component

---
+++ src/lib/components/Heading.svelte
@@ -0,0 +1,12 @@
+<script>
+    let { 
+        text,
+        headingLevel = '2',
+        } = $props();
+ 
+    let headingTag = $derived('h' + headingLevel);
+</script>
+
+<svelte:element this={headingTag}>
+    {text}
+</svelte:element>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Heading from "$lib/components/Heading.svelte";
+</script>
+
+<Heading text="Check your route to Tiptop College" headingLevel="1"></Heading>
+
+<p>At Tiptop College, we offer a number of dedicated college bus services specifically for local students. You can find information about routes, stops, and timetables below.</p>
+
+<h2>Find your stop</h2>
+<p>New student? Not sure which bus stops near you?</p>
+<p>Enter your postcode below to find out if a location close to you is on a Tiptop College bus route.</p>
+
+<h2>All stops</h2>
+<p>This is a list of locations currently served by a Tiptop College bus. Click on a stop to navigate to the full timetable.</p>
+
+<h2>Timetables</h2>
