09a9b5415b4541f9b8d468d8556fc8d64e18fd4b
Emily Sherwood <emily.sherwood@loucoll.ac.uk>
2026-09-07 14:37:20 +0100

    Added in Headings; basic Heading styling

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -1,12 +1,25 @@
 <script>
     let { 
-        text,
-        headingLevel = '2',
+        text = "Heading text",
+        headingLevel = "2",
         } = $props();
  
     let headingTag = $derived('h' + headingLevel);
+    let headingClass = $derived('heading-' + headingLevel);
 </script>
 
-<svelte:element this={headingTag}>
+<svelte:element this={headingTag} class={headingClass}>
     {text}
 </svelte:element>
+
+<style>
+    .heading-1 {
+        font-weight: 900;
+        font-size: 2.5rem;
+    }
+
+    .heading-2 {
+        font-weight: 600;
+        font-size: 1.8rem;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,11 @@
 
 <p>At Tiptop College, we offer a number of dedicated college bus services specifically for local students. You can find information about routes, stops, and timetables below.</p>
 
-<h2>Find your stop</h2>
+<Heading text="Find your stop"></Heading>
 <p>New student? Not sure which bus stops near you?</p>
 <p>Enter your postcode below to find out if a location close to you is on a Tiptop College bus route.</p>
 
-<h2>All stops</h2>
+<Heading text="All stops"></Heading>
 <p>This is a list of locations currently served by a Tiptop College bus. Click on a stop to navigate to the full timetable.</p>
 
-<h2>Timetables</h2>
+<Heading text="Timetables"></Heading>
